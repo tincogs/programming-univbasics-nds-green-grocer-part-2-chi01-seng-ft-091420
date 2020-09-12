1b402021dd0f5bd46af2f8c95f97a10f76c8d090
Tom ODay <tomoday91@gmail.com>
2020-09-12 04:34:40 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -46,7 +46,7 @@ def checkout(cart, coupons)
   total = 0
   counter = 0
   while counter < final_cart.length
-  total += final_cart[counter][:price]
+  total += (final_cart[counter][:price] * final_cart[counter][:count])
   counter += 1
   end
   if total <= 100.00
